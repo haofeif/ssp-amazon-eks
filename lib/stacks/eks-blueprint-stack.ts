58ece810aedeaad21c3171487c84860a6187c608
Andrew Park <andrewpark93@hotmail.com>
2021-08-19 14:54:02 -0700

    pushing updates to opa gatekeeper addon

--- lib/stacks/eks-blueprint-stack.ts
+++ lib/stacks/eks-blueprint-stack.ts
@@ -69,12 +69,7 @@ export class EksBlueprint extends cdk.Stack {
         }
         const clusterProvider = blueprintProps.clusterProvider ?? new EC2ClusterProvider();
 
-<<<<<<< HEAD
         const clusterInfo = clusterProvider.createCluster(this, vpc, blueprintProps.version ?? KubernetesVersion.V1_19);
-=======
-        const clusterInfo = clusterProvider.createCluster(this, vpc, blueprintProps.version ?? KubernetesVersion.V1_20);
-
->>>>>>> aee1f065 (updating cluster version to 1.20)
         const postDeploymentSteps = Array<ClusterPostDeploy>();
         const promises = Array<Promise<cdk.Construct>>();
         const addOnKeys: string[] = [];
